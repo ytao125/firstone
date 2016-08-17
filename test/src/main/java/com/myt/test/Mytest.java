718e2996c33bdd7322df1d27eff2e69f5374672f
Yan Tao <yan.tao@bnymellon.com>
2016-08-17 14:55:37 -0400

    First change

--- test/src/main/java/com/myt/test/Mytest.java
+++ test/src/main/java/com/myt/test/Mytest.java
@@ -3,5 +3,6 @@ package com.myt.test;
 public class Mytest {
 	public void hellow() {
 		System.out.println("Hello! this is my git test");
+		System.out.println("Hello! this is my git test again");
 	}
 }
